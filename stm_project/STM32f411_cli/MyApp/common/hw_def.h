fff4ed9d4092e2f992a6ad6b3a9bf9aad42c0883
Dejavu-Jamaisvu <122339479+Dejavu-Jamaisvu@users.noreply.github.com>
2026-04-16 10:45:15 +0900

    feat temp dma

--- stm_project/STM32f411_cli/MyApp/common/hw_def.h
+++ stm_project/STM32f411_cli/MyApp/common/hw_def.h
@@ -5,7 +5,9 @@
 #include "main.h"
 #include "cmsis_os2.h"
 #include "adc.h"
-
+#include "dma.h"
 // #include "my_gpio.h"
+#include "gpio.h"//잘못적으신거같긴한데
+
 
 #endif //__HW_HW_DEF_H_
--- stm_project/STM32f411_cli/MyApp/hw/driver/temp.c
+++ stm_project/STM32f411_cli/MyApp/hw/driver/temp.c
@@ -1,24 +1,39 @@
 #include "temp.h"
+#include <sys/types.h>
+
+static volatile u_int32_t adc_dma_buf[1];
 
 
 bool tempInit()
 {
+    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_dma_buf, 1);
+
     return true;
 }
+
+
 float tempRead()
 {
-    uint32_t adc_val = 0;
-    float temp_celsius = 0.0f;
 
-    HAL_ADC_Start(&hadc1);
-    if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) {
-        adc_val = HAL_ADC_GetValue(&hadc1);
+    // uint32_t adc_val = 0;
+    // float temp_celsius = 0.0f;
+
+    // HAL_ADC_Start(&hadc1);
+    // if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) {
+    //     adc_val = HAL_ADC_GetValue(&hadc1);
     
-        float vsense = (adc_val/4095.0f)*3.3f;
-        temp_celsius=((vsense-0.76f)/0.0025f)+25.0f;
+    //     float vsense = (adc_val/4095.0f)*3.3f;
+    //     temp_celsius=((vsense-0.76f)/0.0025f)+25.0f;
 
-    }
-    HAL_ADC_Start(&hadc1);
+    // }
+    // HAL_ADC_Start(&hadc1);
+
+
+    uint32_t adc_val = adc_dma_buf[0];
+    float vsense = (adc_val/4095.0f)*3.3f;
+    float temp_celsius=((vsense-0.76f)/0.0025f)+25.0f;
+
+
 
     return temp_celsius;
 }
